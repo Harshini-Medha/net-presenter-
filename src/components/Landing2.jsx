ea3fc51a66259e6fd091fe0239dafdc105f1288b
vishal gehlot <vishalgehlot334@gmail.com>
2023-05-02 16:55:29 +0530

    ok

--- src/components/Landing2.jsx
+++ src/components/Landing2.jsx
@@ -8,21 +8,21 @@ const Landing2 = () => {
   <img  className='text-[20px]   w-[20%] h-[20%] flex phone:w-[50px] ' src='https://netpresenter.wpenginepowered.com/wp-content/uploads/2021/08/icon-data-file-bars-info.svg'/>
   <span className='w-[150px] h-[150px] flex flex-col '>
     <h1 className='text-[40px]  mr-[30px] font-bold flex phone:text-[30px] '><h1 className='text-blue-700'> 83% </h1> Better </h1>
-<h1 className=' w-[300px] text-[25px] phone:text-[20px] '>informed employees</h1></span>
+<h1 className=' w-[300px] phone:w-[200px] text-[25px] phone:text-[20px] '>informed employees</h1></span>
 </div>
 
 <div className='w-[30%] h-[50%] phone:h-[30%] flex  p-[30px] justify-center  phone:mr-[30px]    items-center flex-col'>
   <img  className='text-[20px]   w-[20%] h-[20%] flex  phone:w-[50px] ' src='https://netpresenter.wpenginepowered.com/wp-content/uploads/2021/08/icon-data-file-bars-info.svg'/>
   <span className='w-[150px] h-[150px] flex flex-col '>
     <h1 className='text-[40px] mr-[30px] phone:text-[30px]  font-bold flex '><h1 className='text-blue-700'> 92% </h1> Improved </h1>
-<h1 className=' w-[300px] text-[25px] phone:text-[20px] '>Overall Communication</h1></span>
+<h1 className=' w-[300px]  phone:w-[200px] text-[25px] phone:text-[20px] '>Overall Communication</h1></span>
 </div>
 
 <div className='w-[30%] h-[50%]  phone:h-[30%] flex  p-[30px] justify-center  phone:mr-[30px]     items-center flex-col'>
   <img  className='text-[20px] w-[20%] h-[20%] flex phone:w-[50px]  ' src='https://netpresenter.wpenginepowered.com/wp-content/uploads/2021/08/icon-data-file-bars-info.svg'/>
   <span className='w-[150px] h-[150px] flex flex-col '>
     <h1 className='text-[40px] mr-[30px] font-bold flex phone:text-[30px]'><h1 className='text-blue-700'> 77% </h1> More </h1>
-<h1 className=' w-[300px] text-[25px]  phone:text-[20px]'>engaged employees</h1></span>
+<h1 className=' w-[300px]  phone:w-[200px] text-[25px]  phone:text-[20px]'>engaged employees</h1></span>
 </div>
 
 
